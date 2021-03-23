28629fab694f12d474dfcecb54729dfbfda31159
Austin <austin20texas04@gmail.com>
2021-03-23 08:04:09 -0400

    Initial Commit

---
+++ struct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  struct
+//
+//  Created by Austin Fooce on 3/23/21.
+//  Copyright © 2021 DaFooce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
